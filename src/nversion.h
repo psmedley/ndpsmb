e0c738ace9ffedbe6e504ef6f11acd806d178f45
Paul Smedley <paul@smedley.id.au>
2025-09-20 18:18:16 +1030

    Update samba path
    Signed-off-by: Paul Smedley <paul@smedley.id.au>

--- src/nversion.h
+++ src/nversion.h
@@ -2,11 +2,11 @@
 #ifndef __nversion__H
 #define __nversion__H
 
-#define NDPSMB_VERSION	3.9.0
-#define NDPSMB_BUILD	202208xx
-#define NDPSMB_INSTALL	3\9\0
-#define VERSION		"3.9.0"
-#define BUILD		"202208xx"
+#define NDPSMB_VERSION	3.9.1
+#define NDPSMB_BUILD	20250302
+#define NDPSMB_INSTALL	3\9\1
+#define VERSION		"3.9.1"
+#define BUILD		"20250302"
 #define OS		eCS (OS/2)
 #define ND		NetDrive
 #define VENDOR		"netlabs"
